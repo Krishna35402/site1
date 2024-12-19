ec9ea03ce7b511b996ef2e1d447b31cd4ccdd92e
Krishna35402 <jsdhjsjdkfhs@gmail.com>
2024-12-18 21:11:59 -0800

    Add files via script

---
+++ script.js
@@ -0,0 +1,7 @@
+function openPDF() {
+    window.open("hy.pdf", "_blank"); // Opens the PDF in a new tab
+}
+
+function openPDF1() {
+    window.open("1.txt", "_blank"); // Opens the PDF in a new tab
+}
